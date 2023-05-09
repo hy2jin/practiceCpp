24c6b03991dd2b58ac036c6f264e41746ebf2391
hyejin <hy2jinl22@gmail.com>
2023-05-09 14:40:25 +0900

    혼자 채팅 치기

---
+++ ExamListBox/ExamListBox/ExamListBoxDlg.h
@@ -0,0 +1,38 @@
+
+// ExamListBoxDlg.h : 헤더 파일
+//
+
+#pragma once
+#include "afxwin.h"
+
+
+// CExamListBoxDlg 대화 상자
+class CExamListBoxDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CExamListBoxDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+	enum { IDD = IDD_EXAMLISTBOX_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+private:
+	CListBox m_chat_list;
+public:
+	afx_msg void OnBnClickedInputButton();
+	afx_msg void OnLbnSelchangeChatList();
+};
