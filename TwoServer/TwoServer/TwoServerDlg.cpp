ee48c193c2a6ec2ba7f1c687cf2cbad2baf65347
hyejin <hy2jinl22@gmail.com>
2023-05-10 16:39:46 +0900

    til

--- TwoServer/TwoServer/TwoServerDlg.cpp
+++ TwoServer/TwoServer/TwoServerDlg.cpp
@@ -1,10 +1,10 @@
 
-// ExampleClientDlg.cpp : 구현 파일
+// TwoServerDlg.cpp : 구현 파일
 //
 
 #include "stdafx.h"
-#include "ExampleClient.h"
-#include "ExampleClientDlg.h"
+#include "TwoServer.h"
+#include "TwoServerDlg.h"
 #include "afxdialogex.h"
 
 #ifdef _DEBUG
@@ -43,34 +43,31 @@ BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
 END_MESSAGE_MAP()
 
 
-// CExampleClientDlg 대화 상자
+// CTwoServerDlg 대화 상자
 
 
 
-CExampleClientDlg::CExampleClientDlg(CWnd* pParent /*=NULL*/)
-	: CDialogEx(CExampleClientDlg::IDD, pParent)
+CTwoServerDlg::CTwoServerDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CTwoServerDlg::IDD, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
-void CExampleClientDlg::DoDataExchange(CDataExchange* pDX)
+void CTwoServerDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
 }
 
-BEGIN_MESSAGE_MAP(CExampleClientDlg, CDialogEx)
-//	ON_WM_SYSCOMMAND()
+BEGIN_MESSAGE_MAP(CTwoServerDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
-	ON_BN_CLICKED(IDC_SEND_BUTTON, &CExampleClientDlg::OnBnClickedSendButton)
-	ON_WM_DESTROY()
-	ON_MESSAGE(25001, &CExampleClientDlg::On25001)
 END_MESSAGE_MAP()
 
 
-// CExampleClientDlg 메시지 처리기
+// CTwoServerDlg 메시지 처리기
 
-BOOL CExampleClientDlg::OnInitDialog()
+BOOL CTwoServerDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
@@ -100,41 +97,28 @@ BOOL CExampleClientDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
-	mh_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	struct sockaddr_in srv_addr;
-	memset(&srv_addr, 0, sizeof(struct sockaddr_in));
-	srv_addr.sin_family = AF_INET;
-	srv_addr.sin_addr.s_addr = inet_addr("10.104.192.26");
-	// char ip4[INET_ADDRSTRLEN];
-	// inet_pton(AF_INET, "1270.0.01", &(srv_addr.sin_addr));
-	srv_addr.sin_port = htons(18000);
-
-	WSAAsyncSelect(mh_socket, m_hWnd, 25001, FD_CONNECT);
-	m_connect_flag = 1;
-	connect(mh_socket, (LPSOCKADDR)&srv_addr, sizeof(srv_addr));
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
 
-//void CExampleClientDlg::OnSysCommand(UINT nID, LPARAM lParam)
-//{
-//	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
-//	{
-//		CAboutDlg dlgAbout;
-//		dlgAbout.DoModal();
-//	}
-//	else
-//	{
-//		CDialogEx::OnSysCommand(nID, lParam);
-//	}
-//}
+void CTwoServerDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
 
 // 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
 //  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
 //  프레임워크에서 이 작업을 자동으로 수행합니다.
 
-void CExampleClientDlg::OnPaint()
+void CTwoServerDlg::OnPaint()
 {
 	if (IsIconic())
 	{
@@ -161,48 +145,8 @@ void CExampleClientDlg::OnPaint()
 
 // 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
 //  이 함수를 호출합니다.
-HCURSOR CExampleClientDlg::OnQueryDragIcon()
+HCURSOR CTwoServerDlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-
-
-void CExampleClientDlg::OnBnClickedSendButton()
-{
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-}
-
-
-void CExampleClientDlg::OnDestroy()
-{
-	CDialogEx::OnDestroy();
-
-	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
-	if (mh_socket != INVALID_SOCKET)
-	{
-		closesocket(mh_socket);
-		mh_socket = INVALID_SOCKET;	// 만약을 위해 보정
-	}
-}
-
-void CExampleClientDlg::AddEventString(const char *ap_string)
-{
-	while (m_chat_list)
-}
-
-afx_msg LRESULT CExampleClientDlg::On25001(WPARAM wParam, LPARAM lParam)
-{
-	if (WSAGETSELECTERROR(lParam))	// True:에러발생
-	{
-		m_connect_flag = 0;
-		closesocket(mh_socket);
-		mh_socket = INVALID_SOCKET;
-	}
-	else
-	{
-		m_connect_flag = 2;
-		WSAAsyncSelect(mh_socket, m_hWnd, 25002, FD_READ | FD_CLOSE);
-	}
-	return 0;
-}
